17f10e669784b931d3e6efc80a971b00c524da8f
Bowie <lfranckx@thinkful.com>
2022-04-22 13:21:47 -0500

    remove mobile menu from contact page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -21,24 +21,24 @@ export default function Navbar() {
                         {location.pathname === '/' ? <li><NavLink to='/contact' className={'btn'} >CONTACT</NavLink></li> : <li><NavLink to='/' className={'btn'} >HOME</NavLink></li>}
                     </ul>
 
-                    <div className='toggler' onClick={() => {setActive(!active)}}>
+                    {location.pathname === '/' ? <div className='toggler' onClick={() => {setActive(!active)}}>
                         <div className={`hamburger ${active ? 'active' : ''}`}>
                             <div className='line'></div>
                         </div>
-                    </div>
+                    </div> : <></>}
                 </nav>
                 <div className='banner'>
                     <p>Concerned about the corona virus? <span><Link className='link' activeClass="active" to="pricing" spy={true} smooth={true} offset={0} duration={700}>Online lessons</Link></span> are available</p>
                 </div>
             </div>
-            <nav id='mobile-menu' className={`${active ? 'show' : 'hide'}`}>
+            {location.pathname === '/' ? <nav id='mobile-menu' className={`${active ? 'show' : 'hide'}`}>
                 <ul className='navlinks'>
                     {location.pathname === '/' ? <li><Link className='link' activeClass="active" to="about" spy={true} smooth={true} offset={0} duration={700}>ABOUT US</Link></li> : <li></li>}
                     {location.pathname === '/' ? <li><Link className='link' activeClass="active" to="pricing" spy={true} smooth={true} offset={0} duration={700}>PRICING</Link></li> : <li></li>}
                     <li><Link className='btn-secondary' activeClass="active" to="footer" spy={true} smooth={true} offset={0} duration={700}>JOIN</Link></li>
                     {location.pathname === '/' ? <li><NavLink to='/contact' className={'btn'} >CONTACT</NavLink></li> : <li><NavLink to='/' className={'btn'} >HOME</NavLink></li>}
                 </ul>
-            </nav>
+            </nav> : <></> }
         </>
         
     )
