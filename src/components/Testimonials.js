f73d11979f20321250fc88cc280373e4031a954d
Bowie <lfranckx@thinkful.com>
2022-07-01 17:49:21 -0500

    update testimonials

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -18,13 +18,13 @@ export default function Testimonials() {
                 <div className='content-wrap'>
                     <ul className='content'>
                         <li className='card'>
-                            <div className='img-wrap'><img src={zack}alt='Zack headshot' /></div>
+                            <div className='img-wrap'><img src={jason}alt='Zack headshot' /></div>
                             <div className='text-wrap'>
-                                <h3>Zack</h3>
+                                <h3>Christopher</h3>
                                 <div className='text-left'>
                                     <FaQuoteLeft className='quote-left' />
                                 </div>
-                                <p>My lessons don't feel like an instructor is coming to my house and teaching me how to play the drums. It actually feels like two friends getting together and one is teaching the other how to play.</p>
+                                <p>Jordan is an excellent guitar instructor for our 14 year old. Jordan is patient and provides techniques that make learning the instrument fun. He comes to our house once a week for lessons, he is always on time and professional.</p>
                                 <div className='text-right'>
                                     <FaQuoteRight className='quote-right' />
                                 </div>
@@ -37,20 +37,20 @@ export default function Testimonials() {
                                 <div className='text-left'>
                                     <FaQuoteLeft className='quote-left' />
                                 </div>
-                                <p>My lessons don't feel like an instructor is coming to my house and teaching me how to play the drums. It actually feels like two friends getting together and one is teaching the other how to play.</p>
+                                <p>I can't say enough about Jordan's teaching. He's got incredible chops and knowledge, he asks the right questions to find out right where my knowledge ends and he starts paving the road from there, and he's just a lot of fun to talk about music with. Take lessons from Jordan.</p>
                                 <div className='text-right'>
                                     <FaQuoteRight className='quote-right' />
                                 </div>
                             </div>
                         </li>
                         <li className='card'>
-                            <div className='img-wrap'><img src={jason}alt='Jason headshot' /></div>
+                            <div className='img-wrap'><img src={zack}alt='Jason headshot' /></div>
                             <div className='text-wrap'>
                                 <h3>Jason</h3>
                                 <div className='text-left'>
                                     <FaQuoteLeft className='quote-left' />
                                 </div>
-                                <p>My lessons don't feel like an instructor is coming to my house and teaching me how to play the drums. It actually feels like two friends getting together and one is teaching the other how to play.</p>
+                                <p>Jordan has been a fantastic coach. He is great at creating a laid back atmosphere that is fun, while coming up with creative ways to keep pushing the envelope and improving. Would recommend him to any person looking to learn guitar.</p>
                                 <div className='text-right'>
                                     <FaQuoteRight className='quote-right' />
                                 </div>
